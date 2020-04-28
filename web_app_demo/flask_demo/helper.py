df285b9629694c1898ab6bebc934577cb0877cbe
akim-ruslanov <akim.ruslanov@gmail.com>
2020-04-28 02:18:01 -0700

    completed bare-bones implementation of todo

--- web_app_demo/flask_demo/helper.py
+++ web_app_demo/flask_demo/helper.py
@@ -18,3 +18,57 @@ def add_to_list(item):
     except Exception as e:
         print('Error: ', e)
         return None
+
+def get_all_items():
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        c.execute('select * from items')
+        rows = c.fetchall()
+        return {"count": len(rows), "items": rows}
+    except Exception as e:
+        print('Error: ', e)
+        return None
+
+def get_item(item):
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        c.execute("select status from items where item='%s'" % item)
+        status = c.fetchone()[0]
+        return status
+    except Exception as e:
+        print('Error: ', e)
+        return None
+
+
+def update_status(item, status):
+    if (status.lower().strip() == 'not started'):
+        status = NOT_STARTED
+    elif (status.lower().strip() == 'in progress'):
+        status = IN_PROGRESS
+    elif (status.lower().strip() == 'completed'):
+        status = COMPLETED
+    else:
+        print("Invalid Status: " + status)
+        return None
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        c.execute('update items set status=? where item=?', (status, item))
+        conn.commit()
+        return {item: status}
+    except Exception as e:
+        print('Error: ', e)
+        return None
+
+def delete_item(item):
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        c.execute('delete from items where item=?', (item,))
+        conn.commit()
+        return {'item': item}
+    except Exception as e:
+        print('Error: ', e)
+        return None
