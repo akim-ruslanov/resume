4b6eb8db012b9d259e1dbcd8c4e9f57c6f5b33c6
akim-ruslanov <akim.ruslanov@gmail.com>
2020-04-27 17:47:14 -0700

    Made some file and folder restructuring

--- web_app_demo/flask_demo/app.py
+++ web_app_demo/flask_demo/app.py
@@ -1,16 +1,26 @@
-from flask import Flask
+
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return render_template("index.html")
+    # return "He"
+    # return "He"1
 
 
-@app.route("/<string:name>")
-def hello(name):
-    return f"hello, {name}!"
+# some lines omitted here
 
-
+@app.route("/hello", methods=["POST"])
+def hello():
+    name = request.form.get("name") # take the request the user made, access the form,
+                                    # and store the field called `name` in a Python variable also called `name`
+    print(name)
+    return render_template("hello.html", name=name)
+    # return name
 
 if __name__ == "__main__":
     app.run(debug = True)
